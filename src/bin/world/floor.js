7d22d47bfd9046339080a408fa01d7aa6b0bc2b6
tuttiex <tise.tise@hotmail.com>
2026-09-06 17:06:16 +0100

    chnage game ground

--- src/bin/world/floor.js
+++ src/bin/world/floor.js
@@ -9,8 +9,14 @@ export default class Floor {
     }
 
     createFloor() {
+        const textureLoader = new THREE.TextureLoader();
+        const texture = textureLoader.load('images/forest_ground.png');
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set(10, 3);
+
         this.geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-        this.material = new THREE.MeshPhongMaterial({color: 0xd38650});
+        this.material = new THREE.MeshPhongMaterial({ map: texture, color: 0xffffff });
         this.mesh = new THREE.Mesh(this.geometry, this.material);
 
         this.mesh.position.set(0, -0.1, 0);
